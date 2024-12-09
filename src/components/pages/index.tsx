c2ffe9331668b54c37adbf92bc804c6d92bde83a
Micael Gomes <contato.micaeloficial@gmail.com>
2024-12-09 19:36:25 -0300

    feat: adicionando mais traduções e responsividade na página

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -16,7 +16,6 @@ import { ModeToggle } from "../mode-toggle";
 export const Portfolio = () => {
   const { t } = useTranslation("common");
 
-  // Dados traduzidos dinâmicos
   const professionalExperiences = t("main.recent-enterprises-description", {
     returnObjects: true,
   }) as Record<string, { role: string; enterprise: string; date: string }>;
@@ -30,14 +29,14 @@ export const Portfolio = () => {
       url: "https://cinema-score.vercel.app/",
       title: "Cinema Score",
       description: t("main.projects.cinemaScore.description"),
-      techs: ["React", "Typescript", "Tailwindcss", "Shadcn/ui"],
+      techs: ["React", "Typescript", "TailwindCSS", "Shadcn/ui"],
       img: "cinemascore.png",
     },
     {
       url: "https://trinnocontabilidade.vercel.app/",
       title: "Trinno Contabilidade",
       description: t("main.projects.trinno.description"),
-      techs: ["React", "Typescript", "Tailwindcss"],
+      techs: ["React", "Typescript", "TailwindCSS"],
       img: "trinno.png",
     },
   ];
@@ -54,40 +53,63 @@ export const Portfolio = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-background to-background/80">
-      <div className="container px-4 py-8 mx-auto">
-        {/* Hero Section */}
+      <div className="container px-4 sm:px-6 lg:px-8 py-8 mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between gap-8 mb-12">
-          <div className="w-48 h-48 rounded-full overflow-hidden ring-2 ring-primary/20 flex-shrink-0">
+          <div className="w-32 h-32 md:w-48 md:h-48 max-lg:size-60 rounded-full overflow-hidden ring-2 ring-primary/20 flex-shrink-0">
             <img
-              src="./profilepic.JPEG"
+              src="/profilepic.JPEG"
               alt="Profile Pic"
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="flex-1 space-y-4">
-            <h1 className="text-4xl font-bold tracking-tight">Micael Gomes</h1>
-            <p className="text-xl text-muted-foreground">
+          <div className="flex-1 space-y-6">
+            <h1 className="text-3xl md:text-4xl font-bold tracking-tight">
+              Micael Gomes
+            </h1>
+            <p className="text-lg md:text-xl text-muted-foreground">
               {t("main.description")}
             </p>
-            <div className="flex justify-between gap-4">
-              <div className="flex gap-4">
+            <div className="flex flex-wrap justify-center md:justify-start gap-4">
+              <a
+                href="https://github.com/micaelgomestavares"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Button variant="outline" size="icon">
                   <Github className="h-5 w-5" />
+                  <span className="sr-only">GitHub</span>
                 </Button>
+              </a>
+              <a
+                href="https://linkedin.com/in/micaelgomestavares"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Button variant="outline" size="icon">
                   <Linkedin className="h-5 w-5" />
+                  <span className="sr-only">LinkedIn</span>
                 </Button>
+              </a>
+              <a
+                href="https://x.com/Micazeera"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Button variant="outline" size="icon">
                   <Twitter className="h-5 w-5" />
+                  <span className="sr-only">Twitter</span>
                 </Button>
+              </a>
+              <a href="mailto:contato.micaeloficial@gmail.com">
                 <Button variant="outline" size="icon">
                   <Mail className="h-5 w-5" />
+                  <span className="sr-only">Email</span>
                 </Button>
-                <ModeToggle></ModeToggle>
-                <LanguageSelector></LanguageSelector>
-              </div>
+              </a>
+              <ModeToggle />
+              <LanguageSelector />
             </div>
-            <div className="flex gap-2">
+            <div className="flex flex-wrap gap-2 mt-5 md:justify-start justify-center">
               {technologies.map((tech) => (
                 <Badge key={tech} variant="secondary">
                   {tech}
@@ -96,10 +118,8 @@ export const Portfolio = () => {
             </div>
           </div>
         </div>
-
-        {/* Main Content */}
         <Tabs defaultValue="about" className="space-y-8">
-          <TabsList className="grid w-full md:w-fit grid-cols-3 h-auto gap-4">
+          <TabsList className="grid w-full md:w-fit grid-cols-1 sm:grid-cols-3 h-auto gap-4">
             <TabsTrigger value="about">{t("main.about")}</TabsTrigger>
             <TabsTrigger value="experience">
               {t("main.recent-enterprises")}
@@ -147,12 +167,11 @@ export const Portfolio = () => {
           </TabsContent>
         </Tabs>
 
-        {/* Projects Section */}
         <section className="mt-12">
           <h2 className="text-2xl font-bold mb-6">
             {t("main.projects.title")}
           </h2>
-          <div className="grid grid-cols-3 gap-4 mt-4 max-lg:grid-cols-1">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
             {projects.map((project, index) => (
               <a
                 className="w-full p-2 transition duration-300 ease-in-out hover:scale-105"
